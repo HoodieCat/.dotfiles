12af7ed0d70dcb0ef911c33b3edd31d7a4221835
lijie <lijie_1912@126.cm>
2025-11-26 22:18:30 +0800

    fix gitsings on windows

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -13,7 +13,7 @@ M = {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
-      on_attach = function(bufnr)
+      on_attach = function(_)
           local gitsigns = require("gitsigns")
           local function map(mode, l, r, opts)
             local options = opts or {}
@@ -22,18 +22,18 @@ M = {
             -- enter diff mode
             map("n", "]c", function()
                 if vim.wo.diff then
-                    vim.fn.cmd("normal!  ]c")
+                    vim.cmd.normal({']c', bang = true})
                 else
-                    gitsigns.nav_hunk({"next"})
-                end 
+                    gitsigns.nav_hunk("next")
+                end
             end, {desc = "Jump to next [G]it [C]hange"})
 
             map("n", "[c", function()
                 if vim.wo.diff then
-                    vim.fn.cmd("normal!  [c")
+                    vim.cmd.normal({'[c', bang = true})
                 else
-                    gitsigns.nav_hunk({"prev"})
-                end 
+                    gitsigns.nav_hunk("prev")
+                end
             end, {desc = "Jump to previous [G]it [C]hange"})
 
             --visual mode hunk 
